8739f72c4a6bb31f1e884045dd87693243dfa8e0
Modestas Deksnys <modestasdeksnys98@gmail.com>
2023-06-25 23:35:44 +0300

    created crud to create and delete users

--- src/App.js
+++ src/App.js
@@ -2,22 +2,28 @@ import React from "react";
 import "./styles.css";
 import Home from "./routes/Home";
 import Navbar from "./components/Navbar";
-import { Route, Routes } from "react-router-dom";
+import { Route, Routes, } from "react-router-dom";
 import About from "./routes/About";
 import Service from "./routes/Service";
 import Contact from "./routes/Contact";
+import 'bootstrap/dist/css/bootstrap.min.css';
+import CrudApp from "./crudComponents/CrudApp";
 
 function App() {
   return (
     <div className="App">
+       <Navbar /> 
        <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/about" element={<About />} />
         <Route path="/service" element={<Service />} />
-        <Route path="/contact" element={<Contact />} />
-        </Routes>
-        <Navbar />  
-    </div>
+        <Route path="/contact" element={<Contact />}/>
+        <Route path="/crup" element={<CrudApp />}/>
+      </Routes>
+         <Navbar /> 
+        
+
+        </div>
   );
 }
 
--- src/index.js
+++ src/index.js
@@ -1,5 +1,6 @@
 import ReactDOM from "react-dom";
 import { BrowserRouter } from "react-router-dom";
+import '../node_modules/bootstrap/dist/css/bootstrap.min.css';
 
 import App from "./App";
 
